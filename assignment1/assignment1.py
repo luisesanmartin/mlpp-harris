79f27a0dcedeb83f0716b0e924bf27e971e7cb0f
Luis Eduardo San Martin <lsanmartin@uchicago.edu>
2019-04-07 18:37:42 -0500

    Completed question 1.1

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -1,12 +1,14 @@
 import requests
 
 def data_on_crimes():
+    '''
+    '''
 
     url = "https://data.cityofchicago.org/resource/6zsd-86xi.json?" + \
-          "$where=year = 2017 OR year = 2018"
+          "$where=year = 2017 OR year = 2018&" + \
+          "$limit=600000"
     session = requests.session()
     session.headers.update({'User-Agent': 'Mozilla/5.0'})
     request = session.get(url)
 
     return request.json()
-
