a68cf691f0e529e8d723223fe7b76f51d23c7697
Luis Eduardo San Martin <lsanmartin@uchicago.edu>
2019-04-09 23:52:46 -0500

    This is all I could do

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -12,3 +12,17 @@ def data_on_crimes():
     request = session.get(url)
 
     return request.json()
+
+def data_on_blocks():
+    '''
+    '''
+
+    url = 'https://api.census.gov/data/2017/acs/acs5?get=C02003_001E,' + \
+          'C02003_004E,B19013_001E,B25010_001E,NAME&for=block%20group:*' + \
+          '&in=state:17&in=county:031&in=tract:*'
+
+    session = requests.session()
+    session.headers.update({'User-Agent': 'Mozilla/5.0'})
+    request = session.get(url)
+
+    return request.json()
