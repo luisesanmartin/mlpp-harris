52a49f178267b581c85053a720cbf00dcd21bf78
Luis Eduardo San Martin <lsanmartin@uchicago.edu>
2019-04-16 22:12:22 -0500

    Can't remember what I did

--- assignment2/scripts/util.py
+++ assignment2/scripts/util.py
@@ -21,18 +21,18 @@ def fill_nas(df, column):
 def discretize(df, column):
     '''
     Creates in the dataframe provided dummy variables indicating that an
-    observation belongs to a certain quintile of the column provided.
-    Each dummy has the name of the column + a number indicating the quintile.
+    observation belongs to a certain quartile of the column provided.
+    Each dummy has the name of the column + a number indicating the quartile.
 
     Inputs:
         - column (column of a pandas dataframe): the column we want to
         discretize. It should be a continuous variable included in df.
         - df: the pandas dataframe where column is and where we'll add
-        the new dummy variables
+        the new dummy variables.
 
     Output: nothing. Modifies the df directly.
     '''
-    N_SUBSETS = 5
+    N_SUBSETS = 4
     WIDE = 1 / N_SUBSETS
     
     xtile = 0
@@ -42,29 +42,6 @@ def discretize(df, column):
 
         mini = col.quantile(xtile)
         maxi = col.quantile(xtile + WIDE)
-        df.loc[df[column] < mini, column + str(i)] = 0
-        df.loc[df[column] > maxi, column + str(i)] = 0
         df.loc[(df[column] >= mini) & (df[column] <= maxi), \
-               column + str(i)] = 1
+               column + '_quartile'] = i
         xtile += WIDE
-
-
-def create_dummies(df, column):
-    '''
-    Takes a dataframe (df) and a categorical variable in it (column) and
-    creates a dummy for each distinct value of the input categorical
-    variable.
-
-    Inputs:
-        - column (column of a pandas dataframe): the column we want to
-        discretize. It should be a categorical variable included in df.
-        - df: the pandas dataframe where column is and where we'll add
-        the new dummy variables
-
-    Output: nothing. Modifies the df directly.       
-    '''
-
-    for value in df[column].unique():
-
-        df.loc[df[column] == value, column + str(value)] = 1
-        df.loc[df[column] != value, column + str(value)] = 0
