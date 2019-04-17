89b30b8e1193c1bae8abf19f7369b9194f04b94f
Luis Eduardo San Martin <lsanmartin@uchicago.edu>
2019-04-16 20:44:05 -0500

    Finished pre-processing and attribute-creation functions

---
+++ assignment2/scripts/util.py
@@ -0,0 +1,70 @@
+
+def fill_nas(df, column):
+    '''
+    Replaces the NaN values of a column (column) in a dataframe (df) with
+    the value of the column median
+
+    Inputs:
+        - column (column of a pandas dataframe): the column whose NaN
+        values we want to fill in with the median. It should be a variable
+        included in df.
+        - df: the pandas dataframe where column is and where we'll replace
+        the NaN values.
+
+    Output: nothing. Modifies the df directly.
+    '''
+
+    median = df[column].quantile()
+    df[column] = df[column].fillna(value=median)
+
+
+def discretize(df, column):
+    '''
+    Creates in the dataframe provided dummy variables indicating that an
+    observation belongs to a certain quintile of the column provided.
+    Each dummy has the name of the column + a number indicating the quintile.
+
+    Inputs:
+        - column (column of a pandas dataframe): the column we want to
+        discretize. It should be a continuous variable included in df.
+        - df: the pandas dataframe where column is and where we'll add
+        the new dummy variables
+
+    Output: nothing. Modifies the df directly.
+    '''
+    N_SUBSETS = 5
+    WIDE = 1 / N_SUBSETS
+    
+    xtile = 0
+    col = df[column]
+
+    for i in range(1, N_SUBSETS + 1):
+
+        mini = col.quantile(xtile)
+        maxi = col.quantile(xtile + WIDE)
+        df.loc[df[column] < mini, column + str(i)] = 0
+        df.loc[df[column] > maxi, column + str(i)] = 0
+        df.loc[(df[column] >= mini) & (df[column] <= maxi), \
+               column + str(i)] = 1
+        xtile += WIDE
+
+
+def create_dummies(df, column):
+    '''
+    Takes a dataframe (df) and a categorical variable in it (column) and
+    creates a dummy for each distinct value of the input categorical
+    variable.
+
+    Inputs:
+        - column (column of a pandas dataframe): the column we want to
+        discretize. It should be a categorical variable included in df.
+        - df: the pandas dataframe where column is and where we'll add
+        the new dummy variables
+
+    Output: nothing. Modifies the df directly.       
+    '''
+
+    for value in df[column].unique():
+
+        df.loc[df[column] == value, column + str(value)] = 1
+        df.loc[df[column] != value, column + str(value)] = 0
