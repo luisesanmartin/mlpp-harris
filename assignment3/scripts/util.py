688cfb84d43316404cd5ccd2220403a35784728f
Luis Eduardo San Martin <lsanmartin@uchicago.edu>
2019-04-30 23:32:17 -0500

    Added scripts folder, util.py and coding-analysis-writeup

---
+++ assignment3/scripts/util.py
@@ -0,0 +1,179 @@
+import pandas as pd
+import sklearn.tree as tree
+from sklearn.tree import DecisionTreeClassifier
+
+
+def read(csv_file):
+    '''
+    '''
+
+    return pd.read_csv(csv_file)
+
+
+def columns_list(df):
+	'''
+	'''
+
+	print(df.columns)
+
+
+def columns_types(df):
+	'''
+	'''
+
+	print(df.dtypes)
+
+
+def count_missings(df):
+	'''
+	'''
+
+	total = len(df)
+	for col in df.columns:
+		print(col, 'has', df[col].isna().sum() / total * 100, \
+			'% of missing data points')
+
+
+def tabulate(df, col):
+	'''
+	'''
+
+	print(df.groupby(col).size())
+
+
+def correlations(df):
+	'''
+	'''
+
+	df.corr().style.background_gradient(cmap='coolwarm')
+
+
+def histograms(df):
+	'''
+	'''
+
+	df.hist(figsize=(20, 20))
+
+
+def describe(df):
+	'''
+	'''
+
+	for col in df.columns:
+		print(df[col].describe())
+
+
+def duplicates(df):
+	'''
+	'''
+
+	dups = df[df.duplicated(keep=False)]
+	print(len(dups))
+
+
+def duplicates_in_columns(df, columns):
+	'''
+	'''
+
+	dups = df[df.duplicated(columns, keep=False)]
+	print(len(dups))
+
+
+def fill_nas(df, column):
+    '''
+    Replaces the NaN values of a column (column) in a dataframe (df) with
+    the value of the column median
+
+    Inputs:
+        - column (column of a pandas dataframe): the column whose NaN
+        values we want to fill in with the median. It should be a variable
+        included in df.
+        - df: the pandas dataframe where column is and where we'll replace
+        the NaN values.
+
+    Output: nothing. Modifies the df directly.
+    '''
+
+    median = df[column].quantile()
+    df[column] = df[column].fillna(value=median)
+
+
+def discretize(df, column):
+    '''
+    Creates in the dataframe provided dummy variables indicating that an
+    observation belongs to a certain quartile of the column provided.
+    Each dummy has the name of the column + a number indicating the quartile.
+
+    Inputs:
+        - column (column of a pandas dataframe): the column we want to
+        discretize. It should be a continuous variable included in df.
+        - df: the pandas dataframe where column is and where we'll add
+        the new dummy variables.
+
+    Output: nothing. Modifies the df directly.
+    '''
+    N_SUBSETS = 4
+    WIDE = 1 / N_SUBSETS
+    
+    xtile = 0
+    col = df[column]
+
+    for i in range(1, N_SUBSETS + 1):
+
+        mini = col.quantile(xtile)
+        maxi = col.quantile(xtile + WIDE)
+        df.loc[(df[column] >= mini) & (df[column] <= maxi), \
+               column + '_quartile'] = i
+        xtile += WIDE
+
+
+def create_dummies(df, column):
+    '''
+    Takes a dataframe (df) and a categorical variable in it (column) and
+    creates a dummy for each distinct value of the input categorical
+    variable.
+
+    Inputs:
+        - column (column of a pandas dataframe): the column we want to
+        discretize. It should be a categorical variable included in df.
+        - df: the pandas dataframe where column is and where we'll add
+        the new dummy variables
+    Output: nothing. Modifies the df directly.       
+    '''
+
+    for value in df[column].unique():
+
+        df.loc[df[column] == value, column + '_' + str(value)] = 1
+        df.loc[df[column] != value, column + '_' + str(value)] = 0
+
+
+def replace_over_one(df, column):
+    '''
+    Takes a dataframe (df) and a variable in it (column) and replaces
+    the values over one with ones.
+
+    Inputs:
+        - column (column of a pandas dataframe): the column whose values
+        over one we will replace with ones.
+        - df: the pandas dataframe where column is.
+    Output: nothing. Modifies the df directly.
+    '''
+
+    df.loc[df[column] > 1, column] = 1
+
+
+def discretize_over_zero(df, column):
+    '''
+    Takes a dataframe (df) and a variable in it (column) and creates a
+    dummy indicating the observations that have a value higher than
+    zero.
+
+    Inputs:
+        - column (column of a pandas dataframe): the column whose values
+        we'll take to create the dummy.
+        - df: the pandas dataframe where column is.
+    Output: nothing. Modifies the df directly.
+    '''
+
+    df.loc[df[column] == 0, column + '_over_zero'] = 0
+    df.loc[df[column] > 0, column + '_over_zero'] = 1
